2a7cad04c73b4ea5bcdfe21eea0e03f1ecd500b1
iabalymov <iabalymov@gmail.com>
2022-08-01 22:40:54 +0300

    Created dev01 task

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	// 1. Facade
+	//fmt.Println(pattern.Man.ToDoFacade(pattern.Man{}))
+
+	// 2. Builder
+	//pattern.ToDoBuilderPattern()
+
+	// 4. Factory method
+	//glock, _ := pattern.GetGun("Glock")
+	//mp, _ := pattern.GetGun("MP5")
+	//pattern.FactoryPrint(glock)
+	//pattern.FactoryPrint(mp)
+}
